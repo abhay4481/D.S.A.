50eb13188e5390f085e4887c32cf725d4b47157e
abhay4481 <abhaypratap712@gmail.com>
2022-04-12 19:27:27 +0530

    time pass

--- src/com/company/timepass.java
+++ src/com/company/timepass.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class timepass {
     public static void main(String[] args) {
-        System.out.println("Time pass");
+        System.out.println("Time pass krta rhta h tu");
     }
 }
